d04c1d2dc4e05ac95ccb3b44dcaed86cb6a373d9
AndreySalov <andreysalov@yandex.ru>
2023-12-03 16:35:46 +0300

    first commit

--- ewm-service/src/main/java/ru/yandex/practicum/EwmServiceApp.java
+++ ewm-service/src/main/java/ru/yandex/practicum/EwmServiceApp.java
@@ -1,11 +1,11 @@
-package ru.practicum.explorewithme;
+package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainServerApp {
+public class EwmServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(MainServerApp.class, args);
+        SpringApplication.run(EwmServiceApp.class, args);
     }
 }
